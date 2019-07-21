9a2f4661e40d4e7af887c47a24fd064cc6818798
Daniela Baron <baron.daniela@gmail.com>
2019-07-21 15:33:12 -0400

    [#5] Style bar width proportionally based on value and color code for before and after scenarios

--- src/components/bar-chart/bar-chart.js
+++ src/components/bar-chart/bar-chart.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './bar-chart.css';
 
+function barStyle(valueA, valueB) {
+  const percentage = valueA < valueB ? `${(valueA / valueB) * 100}%` : '100%';
+  // return percentage === '100%'
+  //   ? { width: percentage, 'border-top': 'transparent' }
+  //   : { width: percentage };
+  return {
+    width: percentage,
+  };
+}
+
 function BarChart(props) {
-  const { title } = props;
+  const { title, scenario1, scenario2 } = props;
   return (
     <div className="bar-chart">
       <h2 className="bar-chart--title">{title}</h2>
       <div className="bar-chart--bar-container">
-        <div className="bar-chart--bar bar-chart--bar1" />
-        <div className="bar-chart--bar-label">$2,000</div>
+        <div
+          className="bar-chart--bar bar-chart--bar1"
+          style={barStyle(scenario1, scenario2)}
+        />
+        <div className="bar-chart--bar-label">{scenario1}</div>
       </div>
       <div className="bar-chart--bar-container">
-        <div className="bar-chart--bar bar-chart--bar2" />
-        <div className="bar-chart--bar-label">$1,000</div>
+        <div
+          className="bar-chart--bar bar-chart--bar2"
+          style={barStyle(scenario2, scenario1)}
+        />
+        <div className="bar-chart--bar-label">{scenario2}</div>
       </div>
     </div>
   );
--- src/components/scenarios/scenarios.js
+++ src/components/scenarios/scenarios.js
@@ -2,18 +2,38 @@ import React from 'react';
 import './scenarios.css';
 import BarChart from '../bar-chart/bar-chart';
 
+/*
+<div className="scenarios-data-common">
+  Expected years in retirement: {cashOutBefore.numYrsInRetirement}
+</div>
+*/
+
 function helper(props) {
   const { data } = props;
   const { cashOutBefore, cashOutAfter } = data;
   if (cashOutBefore && cashOutAfter) {
     return (
       <div className="scenarios-data">
-        <div className="scenarios-data-common">
-          Expected years in retirement: {cashOutBefore.numYrsInRetirement}
-        </div>
-        <br />
-        <BarChart title="Annual Income for GIS Eligibility" />
-        <BarChart title="Monthly GIS Entitlement" />
+        <BarChart
+          title="Annual Income for GIS Eligibility"
+          scenario1={cashOutBefore.annualIncome}
+          scenario2={cashOutAfter.annualIncome}
+        />
+        <BarChart
+          title="Monthly GIS Entitlement"
+          scenario1={cashOutBefore.monthlyGIS}
+          scenario2={cashOutAfter.monthlyGIS}
+        />
+        <BarChart
+          title="Annual GIS Entitlement"
+          scenario1={cashOutBefore.annualGIS}
+          scenario2={cashOutAfter.annualGIS}
+        />
+        <BarChart
+          title="Total GIS in Retirement"
+          scenario1={cashOutBefore.totalGISInRetirement}
+          scenario2={cashOutAfter.totalGISInRetirement}
+        />
         <p className="scenarios-subheader">
           If you cash out your RRSP BEFORE retirement:
         </p>

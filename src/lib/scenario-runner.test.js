c065803656f599d270f1ba802fc74da49c715826
Daniela Baron <baron.daniela@gmail.com>
2019-09-15 12:39:32 -0400

    [#20] add scenario test for couple

--- src/lib/scenario-runner.test.js
+++ src/lib/scenario-runner.test.js
@@ -3,7 +3,7 @@ import config from '../config';
 
 describe('scenario runner', () => {
   describe('run', () => {
-    it('runs scenarios for cashing our rrsp before and after retirement', () => {
+    it('runs scenarios for cashing our rrsp before and after retirement for single', () => {
       // Given
       const input = {
         gender: config.DEFAULT_GENDER,
@@ -34,6 +34,37 @@ describe('scenario runner', () => {
       );
 
       expect(result.percentageDecrease).toEqual(48);
+      expect(result).toHaveProperty('chartText');
+    });
+
+    it('runs scenarios for cashing our rrsp before and after retirement for couple', () => {
+      // Given
+      const input = {
+        gender: config.DEFAULT_GENDER,
+        maritalStatus: 'couple',
+        rrsp: 90000,
+        cpp: 4000,
+        pension: 5000,
+        retirementAge: 65,
+      };
+      // When
+      const result = run(input);
+      // Then
+      expect(result.cashOutAfter.numYrsInRetirement).toEqual(20);
+      expect(result.cashOutAfter.annualIncome).toBeCloseTo(13500, 0);
+      expect(result.cashOutAfter.monthlyGIS).toBeCloseTo(450.7, 2);
+      expect(result.cashOutAfter.annualGIS).toBeCloseTo(5408.4, 2);
+      expect(result.cashOutAfter.gisCoverage).toBeDefined();
+      expect(result.cashOutAfter.totalGISInRetirement).toBeCloseTo(108168, 1);
+
+      expect(result.cashOutBefore.numYrsInRetirement).toEqual(20);
+      expect(result.cashOutBefore.annualIncome).toBeCloseTo(9000, 0);
+      expect(result.cashOutBefore.monthlyGIS).toBeCloseTo(638.7, 2);
+      expect(result.cashOutBefore.annualGIS).toBeCloseTo(7664.4, 2);
+      expect(result.cashOutBefore.totalGISInRetirement).toBeCloseTo(153288, 1);
+      expect(result.cashOutBefore.gisCoverage).toBeDefined();
+
+      expect(result.percentageDecrease).toEqual(29);
       expect(result).toHaveProperty('chartText');
     });
   });
